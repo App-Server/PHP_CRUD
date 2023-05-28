759d8cb76bd67a21195cec5b37797a44511414c4
Alcivan Aragao <109984990+App-Server@users.noreply.github.com>
2023-05-27 21:10:57 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = 'localhost';
+    $user = 'root';
+    $password = '1234';
+    $db_name = 'agenda_php';
+
+    $connx = mysqli_connect($host, $user, $password, $db_name);
+
+?>
